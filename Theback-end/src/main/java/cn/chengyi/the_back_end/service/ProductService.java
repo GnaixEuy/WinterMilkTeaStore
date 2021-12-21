c047607ea98d86a0b382e6af3f7651c0db126700
GnaixEuy <sgghcj@live.com>
2021-12-21 15:21:18 +0800

    :poop: ProductDao.java 定义了商品持久层接口;:twisted_rightwards_arrows:合并了一些分支

---
+++ Theback-end/src/main/java/cn/chengyi/the_back_end/service/ProductService.java
@@ -0,0 +1,11 @@
+package cn.chengyi.the_back_end.service;
+
+/**
+ * <img src="http://blog.GnaixEuy.cn/wp-content/uploads/2021/08/bug.jpeg"/>
+ *
+ * @author GnaixEuy
+ * @date 2021/12/21
+ * @see <a href='https://github.com/GnaixEuy'> GnaixEuy的GitHub </a>
+ */
+public interface ProductService {
+}
