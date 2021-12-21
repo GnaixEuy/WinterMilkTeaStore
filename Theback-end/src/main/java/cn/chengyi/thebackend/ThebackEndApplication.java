115b55ad5f8db1520607f66fb6d3e6a4884960d1
GnaixEuy <sgghcj@live.com>
2021-12-21 10:15:51 +0800

    :building_construction: init后端模块

---
+++ Theback-end/src/main/java/cn/chengyi/thebackend/ThebackEndApplication.java
@@ -0,0 +1,13 @@
+package cn.chengyi.thebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThebackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThebackEndApplication.class, args);
+	}
+
+}
---
+++ Theback-end/src/test/java/cn/chengyi/thebackend/ThebackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.chengyi.thebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThebackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
