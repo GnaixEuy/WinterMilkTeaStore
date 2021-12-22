5d3dfcdd319e8fce734ec0307e5986c8ec49917d
GnaixEuy <sgghcj@live.com>
2021-12-22 19:11:58 +0800

    :art: 接入了swagger api生成插件

--- Theback-end/src/main/java/cn/chengyi/the_back_end/ThebackEndApplication.java
+++ Theback-end/src/main/java/cn/chengyi/the_back_end/ThebackEndApplication.java
@@ -1,14 +1,19 @@
 package cn.chengyi.the_back_end;
 
+import cn.chengyi.the_back_end.config.SwaggerProperties;
 import com.github.pagehelper.PageInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 /**
  * @author cy实训项目小组
  */
 @SpringBootApplication
+@EnableConfigurationProperties(value = {
+		SwaggerProperties.class,
+})
 public class ThebackEndApplication {
 
 	public static void main(String[] args) {
