6964152042bba54b492b445ecf7012d1557a56e7
GnaixEuy <sgghcj@live.com>
2021-12-22 11:30:06 +0800

    :building_construction: 完成了Product产品映射表，待单元测试

--- Theback-end/src/main/java/cn/chengyi/the_back_end/dao/ProductDao.java
+++ Theback-end/src/main/java/cn/chengyi/the_back_end/dao/ProductDao.java
@@ -15,22 +15,47 @@ import java.util.List;
 @Mapper
 public interface ProductDao {
 
+	/**
+	 * 通过id查找产品
+	 * @param productId 产品id
+	 * @return product产品
+	 */
 	Product selectProductById(Integer productId);
 
-	Product selectProductByName();
+	Product selectProductByName(String productName);
 
 	/**
 	 * 模糊查询
+	 * @param likeName 模糊查询的字符串
 	 * @return product 模糊查询
 	 */
-	Product vagueSelectProductByName();
+	Product vagueSelectProductByName(String likeName);
 
+	/**
+	 * 查询所有产品
+	 * @return list<product>返回所有的产品集合
+	 */
 	List<Product> findAllProduct();
 
+	/**
+	 * 添加产品
+	 * @param product 产品实体类
+	 * @return 添加的行数
+	 */
 	int addProduct(Product product);
 
-	int deleteProduct(Product product);
+	/**
+	 * 删除产品
+	 * @param productId 要删除的产品id
+	 * @return 返回删除的行数
+	 */
+	int deleteProduct(Integer productId);
 
+	/**
+	 * 更新产品
+	 * @param product 修改后的产品
+	 * @return 返回修改的行数
+	 */
 	int updateProduct(Product product);
 
 }
