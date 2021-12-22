b942d0a160fd8b3142c0545f903c828c95ff2ef8
GnaixEuy <sgghcj@live.com>
2021-12-22 11:16:38 +0800

    :building_construction: 完善了数据库设计，完成了原料映射文件，完成了原料持久层的单元测试

---
+++ Theback-end/src/test/java/cn/chengyi/the_back_end/dao/MaterialDaoTest.java
@@ -0,0 +1,84 @@
+package cn.chengyi.the_back_end.dao;
+
+import cn.chengyi.the_back_end.entity.Material;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * <img src="http://blog.GnaixEuy.cn/wp-content/uploads/2021/08/bug.jpeg"/>
+ *
+ * @author GnaixEuy
+ * @date 2021/12/22
+ * @see <a href='https://github.com/GnaixEuy'> GnaixEuy的GitHub </a>
+ */
+@SpringBootTest
+class MaterialDaoTest {
+
+	@Autowired
+	private MaterialDao materialDao;
+
+	@Test
+	void selectMaterial() {
+		String name = "test1";
+		final Material material = this.materialDao.selectMaterial(name);
+		System.out.println("material.getMaterialName() = " + material.getMaterialName());
+		System.out.println("material.getMaterialStock() = " + material.getMaterialStock());
+		System.out.println("material.getMaterialPrice() = " + material.getMaterialPrice());
+	}
+
+	@Test
+	void findAllMaterial() {
+		final List<Material> allMaterial = this.materialDao.findAllMaterial();
+		for (Material material : allMaterial) {
+			System.out.println("material.getMaterialName() = " + material.getMaterialName());
+			System.out.println("material.getMaterialStock() = " + material.getMaterialStock());
+			System.out.println("material.getMaterialPrice() = " + material.getMaterialPrice());
+			System.out.println("----------------------");
+		}
+	}
+
+	@Test
+	void addMaterial() {
+		String name = "test2";
+		Integer stock = 14;
+		Double price = 697.3;
+		final Material material = new Material(name, stock, price);
+		final Integer integer = this.materialDao.addMaterial(material);
+		if (integer == 1) {
+			System.out.println("成功");
+		} else {
+			System.out.println("失败");
+		}
+	}
+
+	@Test
+	void deleteMaterial() {
+		String name = "test2";
+		final Integer integer = this.materialDao.deleteMaterial(name);
+		if (integer == 1) {
+			System.out.println("成功");
+		} else {
+			System.out.println("失败");
+		}
+	}
+
+	@Test
+	void updateMaterial() {
+		String name = "test1";
+		Material material = this.materialDao.selectMaterial(name);
+		System.out.println("material.getMaterialPrice() = " + material.getMaterialPrice());
+		material.setMaterialPrice(111.0);
+		final Integer integer = this.materialDao.updateMaterial(material);
+		if (integer == 1) {
+			material = this.materialDao.selectMaterial(name);
+			System.out.println("material.getMaterialPrice() = " + material.getMaterialPrice());
+		} else {
+			System.out.println("失败");
+		}
+	}
+}
--- wintermilktea.sql
+++ wintermilktea.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50731
  File Encoding         : 65001
 
- Date: 22/12/2021 09:25:56
+ Date: 22/12/2021 11:13:24
 */
 
 SET NAMES utf8mb4;
@@ -45,16 +45,17 @@ CREATE TABLE `Product` (
   `product_type` varchar(255) DEFAULT NULL,
   `product_price` decimal(10,2) DEFAULT NULL,
   `product_image_id` varchar(255) DEFAULT NULL,
-  `product_material_list` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`product_id`,`product_name`) USING BTREE
+  `product_material_list` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`product_id`,`product_name`) USING BTREE,
+  KEY `product_name` (`product_name`),
+  KEY `产品原料关联` (`product_material_list`),
+  CONSTRAINT `产品原料关联` FOREIGN KEY (`product_material_list`) REFERENCES `material` (`material_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of Product
 -- ----------------------------
 BEGIN;
-INSERT INTO `Product` VALUES (1, '水', '饮品', 2.00, NULL, '水@测试');
-INSERT INTO `Product` VALUES (2, '火', NULL, NULL, NULL, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -64,10 +65,14 @@ DROP TABLE IF EXISTS `comment`;
 CREATE TABLE `comment` (
   `comment_id` varchar(255) NOT NULL,
   `comment_order_id` varchar(255) DEFAULT NULL,
-  `comment_user_id` varchar(0) DEFAULT NULL,
+  `comment_user_id` varchar(255) DEFAULT NULL,
   `comment_content` varchar(255) DEFAULT NULL,
   `comment_finish` tinyint(255) DEFAULT '0' COMMENT '0为false、1为true',
-  PRIMARY KEY (`comment_id`)
+  PRIMARY KEY (`comment_id`),
+  KEY `评价订单关联` (`comment_order_id`),
+  KEY `评价用户关联` (`comment_user_id`),
+  CONSTRAINT `评价用户关联` FOREIGN KEY (`comment_user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `评价订单关联` FOREIGN KEY (`comment_order_id`) REFERENCES `order` (`order_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
@@ -91,6 +96,7 @@ CREATE TABLE `material` (
 -- Records of material
 -- ----------------------------
 BEGIN;
+INSERT INTO `material` VALUES ('test1', 13, 111.00);
 COMMIT;
 
 -- ----------------------------
@@ -106,7 +112,11 @@ CREATE TABLE `order` (
   `order_pay_date_time` datetime(6) DEFAULT NULL,
   `order_is_finish` tinyint(255) DEFAULT '0',
   `order_product_list` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`order_id`)
+  PRIMARY KEY (`order_id`),
+  KEY `订单用户关联` (`order_customer_id`),
+  KEY `用户产品关联` (`order_product_list`),
+  CONSTRAINT `用户产品关联` FOREIGN KEY (`order_product_list`) REFERENCES `Product` (`product_name`),
+  CONSTRAINT `订单用户关联` FOREIGN KEY (`order_customer_id`) REFERENCES `user` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
@@ -126,7 +136,6 @@ CREATE TABLE `user` (
   `user_integral` double(255,0) DEFAULT NULL,
   `user_birthday` date DEFAULT NULL,
   `user_phone` varchar(255) DEFAULT NULL,
-  `user_order_list` varchar(255) DEFAULT NULL,
   `user_image_id` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
